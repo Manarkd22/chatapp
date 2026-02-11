9a1e4a7bf2bb33a18c213fdc2ad25310afc0fc63
Manar Guedidi <manarkd22@gmail.com>
2026-02-11 10:56:12 +0100

    update

--- src/ablyClient.ts
+++ src/ablyClient.ts
@@ -2,13 +2,13 @@
 import * as Ably from "ably";
 import { ChatClient, LogLevel } from "@ably/chat";
 
-// Crée le Realtime client
+//  Realtime client
 export const realtimeClient = new Ably.Realtime({
   key: import.meta.env.VITE_ABLY_API_KEY,
   clientId: "my-first-client",
 });
 
-// Crée le ChatClient pour @ably/chat/react
+// ChatClient pour @ably/chat/react
 export const chatClient = new ChatClient(realtimeClient, {
   logLevel: LogLevel.Info,
 });
